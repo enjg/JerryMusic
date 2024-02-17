fdfc377482c58381c1df13874e9fc834c9a358d4
enjg <201305339@qq.com>
2024-02-17 17:59:59 +0800

    1.0.1

--- axios.js
+++ axios.js
@@ -3,12 +3,12 @@ import mpAdapter from 'axios-miniprogram-adapter';
 
 // 创建一个新的 Axios 实例并设置适配器
 	
-// https://mu-api.yuk0.com
-//http://116.205.238.28:8090
-//
+
+//请提供自己库的API
+let url='';
 const instance = axios.create({
   adapter: mpAdapter,
-  baseURL: 'https://mu-api.yuk0.com',
+  baseURL: url,
    timeout: 5000// 设置你的接口地址
 });
 
--- unpackage/dist/dev/mp-weixin/axios.js
+++ unpackage/dist/dev/mp-weixin/axios.js
@@ -1,8 +1,9 @@
 "use strict";
 const common_vendor = require("./common/vendor.js");
+let url = "";
 const instance = common_vendor.axios.create({
   adapter: common_vendor.mpAdapter,
-  baseURL: "https://mu-api.yuk0.com",
+  baseURL: url,
   timeout: 5e3
   // 设置你的接口地址
 });
